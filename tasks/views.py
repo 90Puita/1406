2bb5b9b946cc8148f18fdb04cc0d141e5db0b1c0
Jarold Meléndez <jarold@ecdesign.es>
2023-06-15 20:24:48 +0200

    añadi lineas

--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,47 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
+
+#from django.http import HttpResponse#
 
 
 def home(request):
     return render(request, 'home.html'), {}
 
 def signup(request):
-
-   if request.method == 'GET':
-        return render(request, 'signup.html'), {
-        'form': UserCreationForm(),})
-        print('sending form')
+    if request.method == 'GET':
+        context = {
+            'form': UserCreationForm
+            }
+        return render(request, 'signup.html', context)
+#cuidar los espacios, deben coincidir las funciones#
     else:
-        if request.POST['password1'] == request.POST['password1']:
+        if request.POST['password1'] == request.POST['password2']:
             #create user#
             try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], 
+                    password=request.POST['password1'])
                 user.save()
-                return HTTPResponse('User created successfully')
-            except: 
-                return HttpResponse('Username already exists')
-        return HttpResponse('Password does not match')
+                login(request, user)
+                return redirect('tasks')
+            
+            except:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm, 
+                    'error': 'Username already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm, 
+            'error': 'Password does not match'
+        })    
+    
+def tasks(request):
+    return render(request, 'tasks.html'), {}
+
 
 
 
